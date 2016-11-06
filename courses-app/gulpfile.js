3d0e3b611db5dc083f2460a6d85f439a4a215234
Jai Srikar <gruprog@gmail.com>
2016-11-06 10:01:18 -0500

    livereloading task

--- courses-app/gulpfile.js
+++ courses-app/gulpfile.js
@@ -18,17 +18,24 @@ var config = {
 gulp.task('connect', function() {
   gulpConnect.server({
     root: [config.paths.dist],
-    port: config.dev.port
+    port: config.dev.port,
+    livereload: true
   });
 });
 
 gulp.task('html', function() {
   gulp.src(config.paths.html)
       .pipe(gulp.dest(config.paths.dist))
+      .pipe(gulpConnect.reload());
 });
 
 gulp.task('watch', function() {
   gulp.watch(config.paths.html, ['html']);
-})
+});
 
-gulp.task('default', ['html', 'connect', 'watch']);
+gulp.task('open', ['connect'], function() {
+  gulp.src('dist/index.html')
+      .pipe(gulpOpen({ uri: config.dev.baseUrl + ':' + config.dev.port + '/'}));
+});
+
+gulp.task('default', ['html', 'open', 'watch']);
