c0180f8cd89b92d76fbf9d40492ce516d5a104e1
Jai Srikar <gruprog@gmail.com>
2016-11-05 21:09:56 -0400

    gulpfile after adding other tasks

--- courses-app/gulpfile.js
+++ courses-app/gulpfile.js
@@ -1,17 +1,34 @@
 "use strict";
 
 var gulp = require('gulp');
+var gulpConnect = require('gulp-connect');
+var gulpOpen = require('gulp-open');
 
 var config = {
+  dev: {
+    port: 8999,
+    baseUrl: 'http://localhost',
+  },
   paths: {
     html: './src/*.html',
     dist: './dist'
   }
 }
 
+gulp.task('connect', function() {
+  gulpConnect.server({
+    root: [config.paths.dist],
+    port: config.dev.port
+  });
+});
+
 gulp.task('html', function() {
   gulp.src(config.paths.html)
       .pipe(gulp.dest(config.paths.dist))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(config.paths.html, ['html']);
 })
 
-gulp.task('default', ['html']);
+gulp.task('default', ['html', 'connect', 'watch']);
